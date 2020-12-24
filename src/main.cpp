3eda8403ab75e67c5e2ac647bd6751634c9b70e9
root <fisch27@gmx.de>
2020-12-24 15:37:48 +0000

    counter

--- src/main.cpp
+++ src/main.cpp
@@ -34,13 +34,15 @@ int main() {
   uWS::Hub h;
 
   PID pid;
+  int count = 0;
+  double cumm_error = 0;
   /**
    * TODO: Initialize the pid variable.
    */
-  pid.Init(1,0.5,0.01);
+  pid.Init(0.01,0.0,0.0);
   
   
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
+  h.onMessage([&pid, &count, &cumm_error](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -67,17 +69,40 @@ int main() {
            */
           pid.UpdateError(cte);
           steer_value = pid.TotalError();
+          if(steer_value > 1.0) steer_value = 1.0;
+          if(steer_value < -1.0) steer_value = -1.0;
+         
+          if(count > 100)
+          {
+            if(count > 200)
+            {
+              // evaluate current parameters, call twiddle
+              
+              
+           
+              count = 0;
+            }
+            else
+            {
+              cumm_error =+ cte * cte;
+            }
+          }
+          
+          
+          
           
           // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
+          std::cout << "count: " << count << "  CTE: " << cte << " Steering Value: " << steer_value 
                     << std::endl;
 
+         count+=1;
+          
           json msgJson;
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = 0.3;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
-          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);          
         }  // end "telemetry" if
       } else {
         // Manual driving
